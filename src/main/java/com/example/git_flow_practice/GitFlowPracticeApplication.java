df88f1772db73c2ca096e1007d54a02bd325af39
wooram <wrjung@syscon.kr>
2023-09-07 09:14:14 +0900

    init

---
+++ src/main/java/com/example/git_flow_practice/GitFlowPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.git_flow_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitFlowPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitFlowPracticeApplication.class, args);
+    }
+
+}
