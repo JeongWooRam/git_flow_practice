a3faca3191a51f1d07890f9606ab22dfbb179578
wooram <wrjung@syscon.kr>
2023-09-07 09:49:43 +0900

    feat: UserRepository 추가

---
+++ src/main/java/com/example/git_flow_practice/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.git_flow_practice.repository;
+
+import com.example.git_flow_practice.domain.User;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository {
+    User findById();
+}
