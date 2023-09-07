56f7c33e50a15365a9648cb80d1abbe182fdb83f
JeongWooRam <143984407+JeongWooRam@users.noreply.github.com>
2023-09-07 14:30:29 +0900

    UserService 추가 및 User 엔티티 id 필드 추가
    * feat: User 엔터티 추가
    
    * User 검색 기능 추가
    
    * feat: User 엔터티 추가
    
    * feat: UserRepository 추가
    
    * fix: UserRepository 이름으로 검색 추가
    
    - findByName() 추가
    
    * feat: UserService 추가

---
+++ src/main/java/com/example/git_flow_practice/domain/User.java
@@ -0,0 +1,10 @@
+package com.example.git_flow_practice.domain;
+
+public class User {
+
+    private Long id;
+
+    private String name;
+
+    private String pw;
+}
---
+++ src/main/java/com/example/git_flow_practice/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.git_flow_practice.repository;
+
+import com.example.git_flow_practice.domain.User;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository {
+    User findById(Long id);
+    User findByName(String name);
+}
